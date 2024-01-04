eef9bc9f6cb6075dd4b024d1c5381a6092b3e5a3
Jared Reich <jaredreich@gmail.com>
2024-01-04 15:00:09 -0700

    Initial commit.

---
+++ src/components/Test.tsx
@@ -0,0 +1,12 @@
+// import { createSignal } from 'solid-js';
+
+import './Test.css';
+
+// const [count, setCount] = createSignal(0);
+export default function Counter() {
+  return (
+    <>
+      <h1>TestComponent</h1>
+    </>
+  );
+}
